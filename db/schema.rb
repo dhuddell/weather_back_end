cd42dc3fce0ebd5913fc11656030969fdec27c72
dhuddell <dan.huddell@gmail.com>
2015-11-12 22:00:31 -0500

    Fixes Schema
    Fixes schema file

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,14 @@ ActiveRecord::Schema.define(version: 20152107203831) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+ create_table "users", force: :cascade do |t|
+    t.string   "email",           null: false
+    t.string   "token",           null: false
+    t.string   "password_digest", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
   create_table "profiles", force: :cascade do |t|
     t.string   "nickname"
     t.string   "fav_meme"
@@ -28,14 +36,6 @@ ActiveRecord::Schema.define(version: 20152107203831) do
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",           null: false
-    t.string   "token",           null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
 
   create_table "weather_pairs", force: :cascade do |t|
     t.string   "weather"
